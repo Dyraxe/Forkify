970aea6fb05ab97af0be73de2f131e15211c347e
Dyraxe <phdja25@gmail.com>
2024-05-12 13:47:02 -0300

    remove unused imports

--- src/js/config.js
+++ src/js/config.js
@@ -1,10 +1,3 @@
-import * as model from './model';
-import addRecipeView from './views/addRecipeView';
-import recipeView from './views/recipeView';
-
-import addRecipeView from './views/addRecipeView';
-import recipeView from './views/recipeView';
-
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SECONDS = 10;
 export const RES_PER_PAGE = 10;
