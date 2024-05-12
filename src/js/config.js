f3ff074cbec3c14b3dfef224c0858ce5758ef855
Dyraxe <phdja25@gmail.com>
2024-05-12 14:51:30 -0300

    changes developer api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SECONDS = 10;
 export const RES_PER_PAGE = 10;
-export const API_KEY = 'a5c9b4a2-6e69-4a10-8b68-5c2e984c3ccc';
+export const API_KEY = '651473bd-f555-4830-9703-cbae0eb7c448';
 export const MODAL_CLOSE_SEC = 3 * 1000;
